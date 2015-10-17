3bf1284d73550feb7a61d6ec59699119ec2fb72e
spolknut <merkel.johnny@gmail.com>
2015-10-17 23:22:57 +0200

    Changed and added functions

--- python/sunset_sunrise_linkoping.py
+++ python/sunset_sunrise_linkoping.py
@@ -1,16 +1,39 @@
-def sunrise():
+def next_sunrise():
         return ephem.localtime(o.next_rising(s))
 
-def sunset():
+def next_sunset():
         return ephem.localtime(o.next_setting(s))
 
+def prev_sunrise():
+        return ephem.localtime(o.previous_rising(s))
+
+def prev_sunset():
+        return ephem.localtime(o.previous_setting(s))
 
 import ephem
 o=ephem.Observer()
 o.lat='58.40117'
 o.long='15.61184'
-s=ephem.Sun()  
-s.compute()  
+s=ephem.Sun()
+s.compute()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
