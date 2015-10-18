007d3567ea78ba85fdc7ca76feac124b1e89ff1f
spolknut <merkel.johnny@gmail.com>
2015-10-18 14:06:02 +0200

    Fixed bug: Off-time where set to 00h.00m, first hour of today.

--- python/updateRelayStates.py
+++ python/updateRelayStates.py
@@ -3,7 +3,7 @@ import os
 import datetime
 from sunrise_sunset_linkoping import next_sunset, next_sunrise, prev_sunset, prev_sunrise
 
-def setRelays(flag) :
+def update_relay_states(flag) :
         print "flag: -", flag, " time: ", datetime.datetime.now()
         os.system("tdtool -" + flag + " 1")
         os.system("tdtool -" + flag + " 2")
@@ -22,11 +22,11 @@ off_sunrise_t = now.replace(hour=8, minute=30, second=0, microsecond=0)
 
 # Friday
 if weekday == 4:
-        off_sunset_t = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        off_sunset_t = now.replace(hour=23, minute=59, second=0, microsecond=0)
 # Saturday
 elif weekday == 5:
         off_sunset_t = now.replace(hour=0, minute=0, second=0, microsecond=0)
-        off_sunrise_t = now.replace(hour=10, minute=0, second=0, microsecond=0)
+        off_sunrise_t = now.replace(hour=23, minute=59, second=0, microsecond=0)
 # Sunday
 elif weekday == 6:
         off_sunrise_t = now.replace(hour=10, minute=0, second=0, microsecond=0)
@@ -48,21 +48,14 @@ print "off with th: ", off_sunset_t + th
 
 #Turn On Morning
 if  on_sunrise_t <= now  and now < (on_sunrise_t + th):
-        setRelays("n")
+        update_relay_states("n")
 #Turn Off Morning
 elif off_sunrise_t <= now and now < (off_sunrise_t + th):
-        setRelays("f")
+        update_relay_states("f")
 #Turn Off Evening
 elif off_sunset_t <= now and now < (off_sunset_t + th):
-        setRelays("f")
+        update_relay_statess("f")
 else :
         print "nothing to be done"
 
 print "Exit"
-
-
-
-
-
-
-
