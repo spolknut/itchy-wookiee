47c8f66862643cf4fe7548f413e7ef53d437d1fa
spolknut <merkel.johnny@gmail.com>
2015-12-12 14:37:32 +0100

    added update sensor data

---
+++ python/update_sensor_data/update_sensor_data.py
@@ -0,0 +1,27 @@
+import re
+from subprocess import PIPE, Popen
+
+def extract_text(reg_filter, text) :
+        m = re.search(reg_filter, text)
+        if m :
+                found = m.group(1)
+                print found
+                return found
+        return ""
+sensor_out = Popen(["tdtool", "--list-sensors"], stdout=PIPE).communicate()[0]
+print "output: ", sensor_out
+
+sensor_id = extract_text("id=(.+?)\s", sensor_out)
+
+sensor_temperature = extract_text("temperature=(.+?)\s", sensor_out)
+
+humidity = extract_text("humidity=(.+?)\s", sensor_out)
+
+time = extract_text("time=(.+?:.+?:.+?)\s", sensor_out)
+
+fo = open("temperature_log_file.txt", "ab")
+fo.write("{id=" +  sensor_id + "} {temperature=" + sensor_temperature +"C} {humidity=" + humidity + "} {time=" + time +"} \n" )
+fo.close()
+
+
+
