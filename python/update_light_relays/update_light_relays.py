58d2bccac418654bea425714df56339f3f0b8952
spolknut <merkel.johnny@gmail.com>
2015-12-13 10:17:46 +0100

    added log file to lights

--- python/update_light_relays/update_light_relays.py
+++ python/update_light_relays/update_light_relays.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env import datetime import time
 import os
 import datetime
+from subprocess import PIPE, Popen
+
 from relay_schedule import is_time_to_turn_on, is_time_to_turn_off
 
 def update_relay_states(flag) :
-        print "flag: -", flag, " time: ", datetime.datetime.now()
-        os.system("tdtool -" + flag + " 1")
-        os.system("tdtool -" + flag + " 2")
-        os.system("tdtool -" + flag + " 3")
-        os.system("tdtool -" + flag + " 4")
+	log_change_to_file("flag: -", flag, " time: ", datetime.datetime.now())
+	os.system("tdtool -" + flag + " 1")
+	os.system("tdtool -" + flag + " 2")
+	os.system("tdtool -" + flag + " 3")
+	os.system("tdtool -" + flag + " 4")
+
+def log_change_to_file(message) :
+	print message
+	fo = open("~/itchy-wookiee/python/update_light_relays/update_light_relays_log.txt", "ab")
+	fo.write("time=" + datetime.datetime.now() + "; " + message + "\n" )
+	fo.close()
 
 def run_relay_schedule(time) :
 	print "time: -", time 
